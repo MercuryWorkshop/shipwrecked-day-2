0d0832e9d94e1d08125dfb1965fa76428a58c536
dora-explora <sarrafdara@gmail.com>
2025-08-10 07:29:30 -0700

    made server broadcast

--- server/main.py
+++ server/main.py
@@ -3,12 +3,16 @@
 import asyncio
 import websockets.asyncio.server as wsserver
 
-async def echo(websocket):
+
+
+async def handler(websocket):
     async for message in websocket:
-        await websocket.send(message)
+        await websocket.broadcast(global_server.connections, message)
 
 async def main():
-    async with wsserver.serve(echo, "localhost", 8765) as server:
-        await server.serve_forever()
+    global global_server
+    async with wsserver.serve(handler, "localhost", 8765) as global_server:
+        # server = global_server
+        await global_server.serve_forever()
 
 asyncio.run(main())
